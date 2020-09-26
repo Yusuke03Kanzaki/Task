22b62c82375e4aa02a6f37376805319ce518690a
Yusuke Kanzaki <hulkmining@gmail.com>
2020-09-27 08:40:59 +0900

    test

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require '../bootstrap.php';
-// require '../MiniBlogApplication.php';
+require '../MiniBlogApplication.php';
 
 // $app = new MiniBlogApplication(false);  //trueがデバックモード
 // $app->run();
