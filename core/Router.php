1f935a638fe901e7f3d7a61a9eefeb9fbc878ec8
Yusuke Kanzaki <hulkmining@gmail.com>
2020-09-27 11:40:05 +0900

    test

--- core/Router.php
+++ core/Router.php
@@ -25,8 +25,9 @@ class Router
         $routes = array();
 
         foreach ($definitions as $url => $params) {
-            $tokens = explode('/', ltrim($url, '/'));
-            foreach ($tokens as $i => $token) {
+            $tokens = explode('/', ltrim($url, '/'));  //explode関数で連想配列のキーの中にある / を削除。ltrim関数で文字列先頭にできた空白を削除。
+            foreach ($tokens as $i => $token) {  //$iは$tokensに入っている配列のインデックス番号が代入される
+                //  strpos関数で$tokenの１文字目が’:’かを調べ、そうであればtrue。substr関数で’:’を削除し$tokenを$nameに代入。$tokenに正規表現形式にした$nameを代入  35
                 if (0 === strpos($token, ':')) {
                     $name = substr($token, 1);
                     $token = '(?P<' . $name . '>[^/]+)';
@@ -34,11 +35,11 @@ class Router
                 $tokens[$i] = $token;
             }
 
-            $pattern = '/' . implode('/', $tokens);
+            $pattern = '/' . implode('/', $tokens);  //implodeで配列の値に’/’を付け結合  "/status/post"  implodeの前に ‘/’ が無いと"status/post"となってしまう
             $routes[$pattern] = $params;
         }
 
-        return $routes;
+        return $routes;  //Routerクラスの変数$routesに代入
     }
 
     /**
