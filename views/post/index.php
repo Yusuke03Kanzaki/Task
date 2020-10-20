e78039bbcf89289d837d8403f92990bf09c5a62b
Yusuke Kanzaki <hulkmining@gmail.com>
2020-10-20 10:57:41 +0900

    test

--- views/post/index.php
+++ views/post/index.php
@@ -22,7 +22,7 @@
               <?php echo $this->render('post/status', array('status' => $status)); ?>
           <?php endforeach; ?>
         <div class="post-preview">
-          <a href="sample-post.php">
+          <a href="<?php echo $base_url; ?>/post/sample">
             <h2 class="post-title">
               Man must explore, and this is exploration at its greatest
             </h2>
@@ -33,45 +33,6 @@
           <p class="post-meta">Posted by
             <a href="#">Start Bootstrap</a>
             on September 24, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="sample-post.php">
-            <h2 class="post-title">
-              I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-            </h2>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 18, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="sample-post.php">
-            <h2 class="post-title">
-              Science has not yet mastered prophecy
-            </h2>
-            <h3 class="post-subtitle">
-              We predict too much for the next year and yet far too little for the next ten.
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on August 24, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="sample-post.php">
-            <h2 class="post-title">
-              Failure is not an option
-            </h2>
-            <h3 class="post-subtitle">
-              Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on July 8, 2019</p>
         </div>
         <hr>
         <!-- Pager -->
